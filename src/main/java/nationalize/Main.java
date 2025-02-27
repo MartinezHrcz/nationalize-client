3f58f6c2694340a9a539146bfd27678c82579838
MartinezHrcz <160034549+MartinezHrcz@users.noreply.github.com>
2025-02-27 14:19:53 +0100

    lombok made

--- src/main/java/nationalize/Main.java
+++ src/main/java/nationalize/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.err.printf("Usage: java %s <name>%n", Main.class.getName());
-            System.exit(1);
+            System.exit(0);
         }
         var client = NationalizeClient.newInstance();
         var nationality = client.getNationality(args[0]);
